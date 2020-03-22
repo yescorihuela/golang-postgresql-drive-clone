375189b12188204470af10ac25ecb606f1351e33
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-22 20:58:39 -0300

    Fix typo router

--- router/router.go
+++ router/router.go
@@ -10,7 +10,9 @@ func New() *echo.Echo {
 	e := echo.New()
 	e.Logger.SetLevel(log.DEBUG)
 	e.Pre(middleware.RemoveTrailingSlash())
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method: ${method}, uri: ${uri}, status=${status}, latency: ${latency_human}, address: ${remote_ip}, time: ${time_rfc3339_nano}\n",
+	}))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
