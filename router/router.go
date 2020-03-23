4bf3a13e9d5fc0456989af0c4315aa0f782a3b33
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-22 21:13:21 -0300

    Replace by rfc3339

--- router/router.go
+++ router/router.go
@@ -11,7 +11,7 @@ func New() *echo.Echo {
 	e.Logger.SetLevel(log.DEBUG)
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method: ${method}, uri: ${uri}, status=${status}, latency: ${latency_human}, address: ${remote_ip}, time: ${time_rfc3339_nano}\n",
+		Format: "method: ${method}, uri: ${uri}, status=${status}, latency: ${latency_human}, address: ${remote_ip}, time: ${time_rfc3339}\n",
 	}))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
