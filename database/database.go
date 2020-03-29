1aa6d8961c05bf5e2da93747506f5dc798e0b39d
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-03-29 17:43:18 -0300

    First docker config is fully functional

--- database/database.go
+++ database/database.go
@@ -5,10 +5,17 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/yescorihuela/golang-postgresql-drive-clone/models"
+	"os"
 )
 
 func New() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=drive-clone password=123456 sslmode=disable")
+	db, err := gorm.Open("postgres",
+		fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DATABASE_HOST"),
+			os.Getenv("POSTGRES_USER"),
+			os.Getenv("POSTGRES_PASSWORD"),
+			os.Getenv("POSTGRES_DB"),
+		))
 	if err != nil {
 		fmt.Println("storage err: ", err)
 	}
